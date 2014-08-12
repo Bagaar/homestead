31995234c606fcba05f23927cc0f0661cc882a1d
tim <tim@tim-G75VW.(none)>
2014-08-12 18:11:51 +0200

    wildcard domain and default vagrant ssh key

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -21,19 +21,19 @@ class Homestead
     config.vm.network "forwarded_port", guest: 5432, host: 54320
 
     # Configure The Public Key For SSH Access
-    config.vm.provision "shell" do |s|
-      s.inline = "echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
-      s.args = [File.read(File.expand_path(settings["authorize"]))]
-    end
+    #config.vm.provision "shell" do |s|
+    #  s.inline = "echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
+    #  s.args = [File.read(File.expand_path(settings["authorize"]))]
+    #end
 
     # Copy The SSH Private Keys To The Box
-    settings["keys"].each do |key|
-      config.vm.provision "shell" do |s|
-        s.privileged = false
-        s.inline = "echo \"$1\" > /home/vagrant/.ssh/$2 && chmod 600 /home/vagrant/.ssh/$2"
-        s.args = [File.read(File.expand_path(key)), key.split('/').last]
-      end
-    end
+    #settings["keys"].each do |key|
+    #  config.vm.provision "shell" do |s|
+    #    s.privileged = false
+    #    s.inline = "echo \"$1\" > /home/vagrant/.ssh/$2 && chmod 600 /home/vagrant/.ssh/$2"
+    #    s.args = [File.read(File.expand_path(key)), key.split('/').last]
+    #  end
+    #end
 
     # Copy The Bash Aliases
     config.vm.provision "shell" do |s|
